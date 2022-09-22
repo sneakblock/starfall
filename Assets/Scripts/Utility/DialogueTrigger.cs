4637cc74a0950e1fb849cb87dc5a0064b7722e96
Elizabeth Liu <esl1350@gmail.com>
2022-09-22 03:08:31 -0400

    barebones structure

---
+++ Assets/Scripts/Utility/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    private String key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
