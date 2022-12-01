52e5b1d6ed7c5bcea55ad8710f27dbc18d3e1563
michelle <michelle_lee100@hotmail.com>
2022-12-01 10:19:21 -0500

    ui stuff

---
+++ Assets/Scripts/UI/AbilityCooldown.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+
+public class AbilityCooldown: MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("1: Dash, 2: Dagger, 3: Clone")]
+    public int abilityNumber;
+
+    private APlayer _player;
+
+    private Slider _slider;
+
+    private AdvancedAbility _currAbility;
+
+    // Set up each slider's values and set the current Link bar to be composed of Safe Link and Standard Link. 
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+
+        _player = GameManager.Instance.aPlayer;
+
+        if (abilityNumber == 1)
+        {
+            _currAbility = _player.GetComponent<DashAbility>();
+        }
+        else if (abilityNumber == 2)
+        {
+            _currAbility = _player.GetComponent<DaggerAbility>();
+        }
+        else if (abilityNumber == 3)
+        {
+            _currAbility = _player.GetComponent<CloneAbility>();
+        }
+
+    }
+
+    void Update()
+    {
+        if (_currAbility.castCompleted)
+        {
+            UpdateSliderCooldown();
+        }
+        else
+        {
+            UpdateSliderCast();
+        }
+        
+    }
+
+    public void UpdateSliderCooldown()
+    {
+        _slider.value = 1 - (_currAbility.cooldownTimer / _currAbility.cooldownTime);
+    }
+
+    public void UpdateSliderCast()
+    {
+        _slider.value = _currAbility.castTimer / _currAbility.castTime;
+
+    }
+
+}
--- Assets/Scripts/UI/LinkBar.cs
+++ Assets/Scripts/UI/LinkBar.cs
@@ -5,137 +5,39 @@ using UnityEngine.UI;
 
 public class LinkBar : MonoBehaviour
 {
-    // List containing a slider for each Link type.
-    // [0]: Safe Link
-    // [1]: Standard Link
-    // [2]: Excess Link
-    public List<Slider> _LinkSliders = new List<Slider>();
-    // Max values for each Link type. Total Link bar defaults to a max value of 200.
-    public float _maxSafeLink = 10, _maxStandardLink = 100, _maxExcessLink = 200;
-
-    // The current Link slider in effect.
-    private float _currentLink;
-    private Slider _currLinkSlider;
-    private int _currSliderNum;
+    private APlayer _player;
+    private Slider _slider;
 
     // Set up each slider's values and set the current Link bar to be composed of Safe Link and Standard Link. 
     void Start()
     {
-        SetLinkBarThresholds(_LinkSliders[0], 0, _maxSafeLink);
-        SetLinkBarThresholds(_LinkSliders[1], _maxSafeLink, _maxStandardLink);
-        SetLinkBarThresholds(_LinkSliders[2], _maxStandardLink, _maxExcessLink);
+        _slider = GetComponent<Slider>();
 
-        SetLinkBarVals(_LinkSliders[0], _maxSafeLink);
-        SetLinkBarVals(_LinkSliders[1], _maxStandardLink);
-        SetLinkBarVals(_LinkSliders[2], 0);
+        _player = GameManager.Instance.aPlayer;
 
-        _currSliderNum = 1;
-        _currLinkSlider = _LinkSliders[_currSliderNum];
-        _currentLink = _maxStandardLink;
     }
 
     // TEMP: manipulate the Link bar's values.
     void Update()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            RemoveLink(30f);
-        }
-        else if (Input.GetKeyDown(KeyCode.G))
-        {
-            AddLink(30f);
-        }
-        */
+
+
     }
 
-    // Remove a certain amount of Link from the Link bar.
-    public void RemoveLink(float linkAmt)
-    {  
-        //for some reason this isn't registered as being in Start() when using UnityEvents
-        _currLinkSlider = _LinkSliders[_currSliderNum];
-        // Update the player's total amount of Link
-        if (_currentLink - linkAmt < 0)
-        {
-            _currentLink = 0;
-            // KILL PLAYER
-        }
-        else
-        {
-            _currentLink -= linkAmt;
-        }
-
-        // if the deducted amount of Link would cross over into the preceding Link region, switch the active Link bar to the preceding one
-        if (_currLinkSlider.value - linkAmt < _currLinkSlider.minValue)
-        {
-            float leftoverLink = _currLinkSlider.minValue - (_currLinkSlider.value - linkAmt);
-            UpdateLinkBar(_currLinkSlider.minValue);
-            //_currLinkSlider.value = _currLinkSlider.minValue;
-            if (_currSliderNum > 0)
-            {
-                _currSliderNum--;
-                _currLinkSlider = _LinkSliders[_currSliderNum];
-                RemoveLink(leftoverLink);
-            }
-            // else, just deduct the Link and display it accordingly on the current active Link region 
-        }
-        else
-        {
-            UpdateLinkBar(_currLinkSlider.value - linkAmt);
-            //_currLinkSlider.value -= linkAmt;
-        }
-    }
-
-    // Add a certain amount of Link to the Link Bar.
-    public void AddLink(float linkAmt)
+    public void UpdateLink()
     {
-        //for some reason this isn't registered as being in Start() when using UnityEvents
-        _currLinkSlider = _LinkSliders[_currSliderNum];
-        // Update the player's total amount of Link
-        if (_currentLink + linkAmt > _maxExcessLink)
-        {
-            _currentLink = _maxExcessLink;
-        }
-        else
-        {
-            _currentLink += linkAmt;
-        }
-
-        // if the added amount of Link would cross over into the next Link region, switch the active Link bar to the next one
-        if (_currLinkSlider.value + linkAmt > _currLinkSlider.maxValue)
-        {
-            float leftoverLink = (_currLinkSlider.value + linkAmt) - _currLinkSlider.maxValue;
-            //_currLinkSlider.value = _currLinkSlider.maxValue;
-            UpdateLinkBar(_currLinkSlider.maxValue);
-            if (_currSliderNum < 2)
-            {
-                _currSliderNum++;
-                _currLinkSlider = _LinkSliders[_currSliderNum];
-                AddLink(leftoverLink);
-            }
-        }
-        // else, just add the Link and display it accordingly on the current active Link region 
-        else
-        {
-            UpdateLinkBar(_currLinkSlider.value + linkAmt);
-            //_currLinkSlider.value += linkAmt;
-        }
+        _slider.value = _player.health / _player.maxHealth;
     }
 
-    // HELPER FUNCTIONS //
-    private void UpdateLinkBar(float newLinkAmt)
+    public void OnEnable()
     {
-        _currLinkSlider.value = newLinkAmt;
+        APlayer.OnUpdateLink += UpdateLink;
     }
 
-    private void SetLinkBarThresholds(Slider linkSlider, float minLink, float maxLink)
+    public void OnDisable()
     {
-        linkSlider.minValue = minLink;
-        linkSlider.maxValue = maxLink;
+        APlayer.OnUpdateLink -= UpdateLink;
     }
 
-    private void SetLinkBarVals(Slider linkSlider, float linkAmt)
-    {
-        linkSlider.value = linkAmt;
-    }
+
 }
