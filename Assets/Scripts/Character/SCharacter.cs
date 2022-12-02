370c1902aa123ea47b37ebb4d0a7c2b235285553
block3 <block3@gatech.edu>
2022-12-02 12:53:45 -0500

    fix the HELL issue

--- Assets/Scripts/Character/SCharacter.cs
+++ Assets/Scripts/Character/SCharacter.cs
@@ -97,6 +97,7 @@ public abstract class SCharacter : MonoBehaviour, IDamageable, ICharacterControl
     void Start()
     {
         abilityManager = new AbilityManager();
+        motor.CharacterController = this;
         maxHealth = health;
         Collider = GetComponent<Collider>();
         
--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Tank : Grunta
 {
     //He's bigger.
+
+    void Foo()
+    {
+        base.StartCharacter();
+    }
 }
