11247dd4d069d71055b57fa38dde60cbd38edaa9
block3 <block3@gatech.edu>
2022-12-02 08:20:46 -0500

    Add flyer, grunta polish. pre kuze ragdoll unpack

---
+++ Assets/EnergyBall.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnergyBall : MonoBehaviour
+{
+    public GameObject impactEffect;
+    public float explosionRadius;
+    public float damage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        var effectInstance = Instantiate(impactEffect, transform.position, Quaternion.identity);
+        Destroy(effectInstance, 1f);
+
+        if (GameManager.Instance.aPlayer)
+        {
+            var player = GameManager.Instance.aPlayer;
+            if (Vector3.Distance(transform.position, player.transform.position) <= explosionRadius)
+            {
+                player.Damage(damage);
+            }
+        }
+        
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
--- Assets/Scripts/Weapon/Weapons/StandardBulletWeapon.cs
+++ Assets/Scripts/Weapon/Weapons/StandardBulletWeapon.cs
@@ -16,25 +16,15 @@ public class StandardBulletWeapon : RangedWeapon
 
     protected override void Fire(Vector3 dir)
     {
+        base.Fire(dir);
         foreach (var barrelTransform in barrelTransforms)
         {
             GameObject projectile = Instantiate(weaponData.bullet, barrelTransform.position, Quaternion.identity);
-            projectile.tag = gameObject.tag;
             projectile.transform.forward = dir.normalized;
             var rb = projectile.GetComponent<Rigidbody>();
-            var coll = projectile.GetComponent<Collider>();
-            Debug.Log($"ownerchar is {OwnerChar.gameObject.name}");
-            //The bullets collide with the owner of the gun and there is a "recoil" as a result. This is a result of the backend? and that should be solved at a later date.
-            Physics.IgnoreCollision(coll, OwnerChar.gameObject.GetComponent<Collider>(), true);
-            //This is temp, real functionality should ignore collision with other bullets and the entity that fires the weapon, but not all enemies for example if the weapon is fired by an enemy
-            _projectiles.Add(projectile);
-            foreach (var p in _projectiles)
-            {
-                Physics.IgnoreCollision(coll, p.GetComponent<Collider>(), true);
-            }
             rb.AddForce(dir.normalized * weaponData.firingForce, ForceMode.Impulse);
         }
-        PlayFireEffect();
+        // PlayFireEffect();
     }
     
     public void PlayFireEffect()
